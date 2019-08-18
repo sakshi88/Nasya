a7b12b534cf9b150d287ed5f0a48a20bd3e1eb14
sakshi <sakshikataria59@yahoo.com>
2019-08-18 13:36:52 +0530

    Completed whyChooseUs section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { WhyChooseUsComponent } from './landing/why-choose-us/why-choose-us.component';
+import { OurServicesComponent } from './landing/our-services/our-services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    WhyChooseUsComponent
+    WhyChooseUsComponent,
+    OurServicesComponent
   ],
   imports: [
     BrowserModule
--- src/app/landing/why-choose-us/why-choose-us-array.ts
+++ src/app/landing/why-choose-us/why-choose-us-array.ts
@@ -3,22 +3,23 @@ import { WhyChooseUs } from './why-choose-us';
 export const WhyChooseUsArray: WhyChooseUs[] = [
     {
         imgUrl: '/assets/expertise1.png', 
+        textFront: 'Our Expertise', 
+        textBack: 'Our reputation for outstanding clinical care and friendly atmosphere with continuous investment'
+    },
+    {
+        imgUrl: '/assets/quality.png', 
         textFront: 'Cost Effective Quality Services', 
-        textBack: 'Nasya is committed to provide efficient, high Quality, cost-efficient healthcare services'
+        textBack: 'NASYA is committed to provide efficient, high Quality, cost-efficient healthcare services'
     },
     {
-        imgUrl: '/assets/expertise1.png', 
+        imgUrl: '/assets/strategic.png', 
         textFront: 'Our Strategic Investment', 
-        textBack: 'Nasya is committed to investing in excellence. We are having a team with full of experience and expertise in their domain'
+        textBack: 'NASYA is committed to investing in excellence to provide our patient the best possible treatment'
     },
     {
-        imgUrl: '/assets/expertise1.png', 
-        textFront: 'Our Quality and outcomes', 
-        textBack: 'NASYA strives to be the leading provider of independent healthcare, excelling in the provision of quality healthcare services and is committed to the highest quality standards of patient care. Nasya’s approach to quality informs our daily decisions as we aim to continually improve and excel in the all we do'
-    },
-    {
-        imgUrl: '/assets/expertise1.png', 
-        textFront: 'Recommended by people like you', 
-        textBack: 'Nasya healthcare greatly values and continually monitors patient’s feedback, so that we can be responsive/ continually learn and improve our service as necessary. As it is our motive to deliver patient-centered care at all time'
+        imgUrl: '/assets/outcome.png', 
+        textFront: 'Our Quality and Outcome', 
+        textBack: 'NASYA strives to be the leading provider of independent healthcare and excelling with quality healthcare services'
     },
 ]  
+
