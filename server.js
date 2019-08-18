3d53226b87e5cb7d1d62ff2a93050a0eab73f0cb
sakshi <sakshikataria59@yahoo.com>
2019-08-18 22:51:53 +0530

    Added the whole landing page

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/nasya'));
 
-app.get('/*', function(req, res) {
+app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/nasya/index.html'));
 });
 
